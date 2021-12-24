7497693689f8dcb138c62c99432300ed45b0a718
vivekyadav07 <vivekkumaryadav97@gmail.com>
2021-12-24 16:50:02 +0530

    final commit

--- book/urls.py
+++ book/urls.py
@@ -6,7 +6,8 @@ from .views import SearchResultsListView
 urlpatterns = [
    path('' ,views.index),
    path('booklist1/',views.BookListView,name="booklist1"),
-
+   path('topval/',views.BookListView1,name="topval"),
+   path('top/',views.BookListView2,name="top"),
    path("add_book/", views.add_book, name="add_book"),
    path("update_data/<int:id>/", views.update_data, name='update_data'),
    
--- book/views.py
+++ book/views.py
@@ -4,8 +4,7 @@ from .forms import *
 from django.db.models import Q
 from django.views.generic import  ListView
 
-#from .forms import Bookform
-
+from django.db.models import Count
 #Create your views here.
 
 # HOME PAGE
@@ -16,27 +15,23 @@ def index(request):
 
 
 
-
-
 # VIEW THAT WILL RETURN LIST OF ALL BOOKS IN LIBRARY
 def BookListView(request):
     book_list = Book.objects.all()
     context ={'booklist':book_list}
-    # MODELNAME.objects.all() is used to get all objects i.e. tuples from database
     return render(request, 'book_list.html', context)
 
 
 
-#ADD BOOK
 
+
+#ADD BOOK
 def add_book(request):
     if request.method == "POST":
         title = request.POST['title']
         author = request.POST['author']
         summary = request.POST.get('summary')
         isbn = request.POST['isbn']
-       
-
         books = Book.objects.create(title=title, author=author,summary=summary,isbn=isbn)
         books.save()
        
@@ -44,20 +39,20 @@ def add_book(request):
     return render(request, "add_book.html")
 
 
-  #UPDATE BOOK
+
+
+
+#UPDATE BOOK
 def update_data(request,id):
-
-
     if request.method == "POST":
 
         pi=Book.objects.get(pk=id)
         fm =Bookform(request.POST,instance=pi)
         if fm.is_valid():
          fm.save()
-
+         return render(request, "update_data.html")
+    
     else:
-
-
         pi=Book.objects.get(pk=id)
         fm =Bookform(instance=pi)
     return render (request,'update_data.html',{'form':fm})       
@@ -68,7 +63,7 @@ def update_data(request,id):
 
 #DELETE BOOK
 
-def delete_book(request, myid):
+def delete_book( request,myid):
     books = Book.objects.filter(id=myid)
     books.delete()
     return redirect("/booklist1")
@@ -80,6 +75,30 @@ class SearchResultsListView(ListView):
 	model = Book
 	template_name = 'search_results.html'
 
-	def get_queryset(self): # new
+	def get_queryset(self): 
 		query = self.request.GET.get('q')
 		return Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query))
+
+'''def topval(request):
+   
+    Book.objects.values('id').annotate(title_count=Count('title')).order_by('-title_count')[:5]
+   # book_ids = Book.objects.filter(author=author).values_list('id', 'title').order_by('title')
+    top_records = (Book.objects.order_by('-title').filter(score__in=top_scores[:10]))'''
+
+#TOP THREE VALUE
+def BookListView1(request):
+   # book_list = Book.objects.all()
+    book_list = Book.objects.annotate(Count('isbn')).order_by('isbn__count')[:3]
+  
+    #print(book_list)
+    context ={'booklist':book_list}
+    return render(request, 'top.html', context)
+
+#TOP VALUE
+def BookListView2(request):
+   # book_list = Book.objects.all()
+    book_list2 = Book.objects.annotate(Count('isbn')).order_by('isbn__count')[:1]
+    
+    #print(book_list)
+    context ={'booklist':book_list2}
+    return render(request, 'top.html', context)
